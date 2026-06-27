d9be95bea7a0e04648e46c14c8bf8b49ae276e82
Altap Shaikh <altap_shaikh111@rediffmail.com>
2026-06-27 19:36:08 +0530

    Add files via upload

---
+++ aws_cli_install.sh
@@ -0,0 +1,7 @@
+sudo apt update -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt update
+sudo apt install unzip -y
+unzip awscliv2.zip
+sudo ./aws/install
+aws --version
