4b503b23ec8d0ec250b743dc110b0426fc360c2a
Altap Shaikh <altap_shaikh111@rediffmail.com>
2026-06-27 20:01:24 +0530

    Update aws_cli_install.sh

--- aws_cli_install.sh
+++ aws_cli_install.sh
@@ -4,3 +4,9 @@ sudo apt update
 sudo apt install unzip -y
 unzip awscliv2.zip
 sudo ./aws/install
+echo "installing kubectl"
+curl -LO https://dl.k8s.io/release/v1.31.0/bin/linux/amd64/kubectl 
+ 
+chmod +x kubectl 
+ 
+sudo mv kubectl /usr/local/bin/
